e114c47aaf1e8e57d0d11fe459d7c40a4c536530
Ellis Clayton <ellis@ellis.codes>
2026-04-27 21:02:42 +1000

    Add number arguments for initial value
    These are now required by ESPHome

--- components/mcp4xxx/__init__.py
+++ components/mcp4xxx/__init__.py
@@ -1,7 +1,6 @@
 import esphome.codegen as cg
 from esphome.components import spi, number
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_MIN_VALUE, CONF_MAX_VALUE, CONF_STEP
 
 CODEOWNERS = ["@ellsclytn"]
 DEPENDENCIES = ["spi"]
@@ -9,6 +8,7 @@ AUTO_LOAD = ["number", "fan"]
 MULTI_CONF = True
 
 CONF_INITIAL_VALUE = "initial_value"
+MCP4XXX_MAX_VALUE = 128
 
 mcp4xxx_ns = cg.esphome_ns.namespace("mcp4xxx")
 MCP4XXX = mcp4xxx_ns.class_("MCP4XXX", number.Number, cg.Component, spi.SPIDevice)
@@ -17,7 +17,9 @@ CONFIG_SCHEMA = (
     number.number_schema(MCP4XXX)
     .extend(
         {
-            cv.Optional(CONF_INITIAL_VALUE, default=0): cv.int_range(min=0, max=128),
+            cv.Optional(CONF_INITIAL_VALUE, default=0): cv.int_range(
+                min=0, max=MCP4XXX_MAX_VALUE
+            ),
         }
     )
     .extend(cv.COMPONENT_SCHEMA)
@@ -28,6 +30,9 @@ CONFIG_SCHEMA = (
 async def to_code(config):
     var = await number.new_number(
         config,
+        min_value=0,
+        max_value=MCP4XXX_MAX_VALUE,
+        step=1,
     )
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
